e7ac127e8dd2a3127d93dd527e58688e38611aab
cobr3n <vladislav91@mail.ru>
2017-10-01 23:06:44 +0700

    end 6 lesson

---
+++ app/app.component.ts
@@ -0,0 +1,36 @@
+import { Component } from 'angular2/core';
+import {HeroService} from './hero.service';
+import {HeroesComponent} from './heroes.component';
+import {RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from 'angular2/router';
+import {DashboardComponent} from './dashboard.component';
+
+@Component({
+    selector: 'my-app',
+    template: `
+        <h1>{{title}}</h1>
+        <nav>
+            <a [routerLink]="['Dashboard']">Dashboard</a>
+            <a [routerLink]="['Heroes']">Heroes</a>
+        </nav>
+        <router-outlet></router-outlet>
+    `,
+    directives: [ROUTER_DIRECTIVES],
+    providers: [HeroService, ROUTER_PROVIDERS]
+})
+@RouteConfig([
+    {
+        path: '/heroes',
+        name: 'Heroes',
+        component: HeroesComponent,
+        useAsDefault: true
+    },
+    {
+        path: '/dashboard',
+        name: 'Dashboard',
+        component: DashboardComponent
+
+    }
+])
+export class AppComponent {
+    title = 'Tour of Heroes';
+}
